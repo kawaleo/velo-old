c28649aef3e15d01869b98f4a8d0437f64fba1f5
kawaleo <bradyhirschey1@gmail.com>
2024-01-06 22:24:25 -0500

    add .gitignore

---
+++ target/debug/deps/velo.d
@@ -0,0 +1,13 @@
+C:\Users\brady\Desktop\Projects\rust\fun\velo\target\debug\deps\velo.exe: src\main.rs src\syntax\mod.rs src\syntax\ast.rs src\syntax\error.rs src\syntax\lexer.rs src\syntax\parse\mod.rs src\syntax\parse\stmt\mod.rs src\syntax\parse\stmt\function.rs src\syntax\parse\stmt\variable.rs
+
+C:\Users\brady\Desktop\Projects\rust\fun\velo\target\debug\deps\velo.d: src\main.rs src\syntax\mod.rs src\syntax\ast.rs src\syntax\error.rs src\syntax\lexer.rs src\syntax\parse\mod.rs src\syntax\parse\stmt\mod.rs src\syntax\parse\stmt\function.rs src\syntax\parse\stmt\variable.rs
+
+src\main.rs:
+src\syntax\mod.rs:
+src\syntax\ast.rs:
+src\syntax\error.rs:
+src\syntax\lexer.rs:
+src\syntax\parse\mod.rs:
+src\syntax\parse\stmt\mod.rs:
+src\syntax\parse\stmt\function.rs:
+src\syntax\parse\stmt\variable.rs:
